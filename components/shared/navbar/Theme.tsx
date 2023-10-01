c6c153d8b13f65797a4eaf4a3683f05d650e30a9
Ribaud Hugo <hugo.ribaud@cap4lab.com>
2023-10-01 13:30:32 +0200

    added cursor-pointer to Theme

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -20,7 +20,7 @@ const Theme = () => {
   return (
     <Menubar className="relative border-none bg-transparent shadow-none">
       <MenubarMenu>
-        <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+        <MenubarTrigger className="cursor-pointer focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
           {mode === 'light' ? (
             <Image
             src="/assets/icons/sun.svg" alt="sun"
@@ -39,7 +39,7 @@ const Theme = () => {
           {themes.map((item) => (
             <MenubarItem
               key={item.value}
-              className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
+              className="flex cursor-pointer items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
               onClick={() => {
                 setMode(item.value)
 
